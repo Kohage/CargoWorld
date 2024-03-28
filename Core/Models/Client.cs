c943c62a11f276839d297634c1f44b5a76356351
Kohage <89828621077@bk.ru>
2024-03-28 20:54:57 +0300

    Initial

--- Core/Models/Client.cs
+++ Core/Models/Client.cs
@@ -1,43 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.ComponentModel.DataAnnotations;
+
 using Microsoft.AspNetCore.Identity;
 
-namespace Core.Models
+
+namespace DAL
 {
-    public class Client : Microsoft.AspNetCore.Identity.IdentityUser
+    public class AppUser : IdentityUser<Guid>
     {
-        [Required]
-        [Key]
-        public int Client_Id { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
-        public string Name { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
-        public string Surname { get; set; }
-        public string Adress { get; set; }
-        public string Number { get; set; }
-        [Required]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
-        public string Email { get; set; }
-        [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
-        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
 
-
-        [NotMapped]
-        [Required]
-        [System.ComponentModel.DataAnnotations.Compare("Password")]
-        public string ConfirmPassword { get; set; }
-        public string FullName()
+        public string FullName
         {
-            return this.Name + " " + this.Surname;
+            get
+            {
+                return FirstName + " " + SecondName;
+            }
+        }
+
+
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public Guid UserSettingsId { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public virtual UserSettings UserSettings { get; set; } = new();
+
+        public override string ToString()
+        {
+            return UserName;
         }
     }
 }
---
+++ Core/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Core.Models
+{
+    /// <summary>
+    ///     DTO для аутентификации
+    /// </summary>
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
